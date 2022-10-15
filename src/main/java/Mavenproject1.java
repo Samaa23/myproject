6e0ee844c854106adf8993be992d5c5d8feb5c2a
unknown <muhammedalaa2213@gmail.com>
2022-10-16 01:29:07 +0200

    test

--- src/main/java/Mavenproject1.java
+++ src/main/java/Mavenproject1.java
@@ -12,8 +12,7 @@ public class Mavenproject1 {
     public static void main(String[] args) {
         System.out.println("Hllo World!");
         System.out.println("Hllo Woasdasrld!");
-
+        
         System.out.println("Hello World!");
-        int x = 5;
     }
 }
